d458d8074c7496f0854db8e32b1ce1121dbb5dbe
lic <zheyuan54@qq.com>
2022-03-20 08:09:20 +0800

    feat: 改写页面布局及路由

---
+++ src/components/container/src/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-container>
+    <el-aside width="200px">Aside</el-aside>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,22 @@
-import { createRouter, createWebHistory, createWebHashHistory, RouteRecordRaw } from 'vue-router';
+import { createRouter, createWebHistory, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'Home',
-    meta: {
-      title: '首页',
-      keepAlive: true
-    },
-    component: () => import('../views/Home.vue'),
-  },
-];
+    component: () => import('../components/container/src/index.vue'),
+    children: [
+      {
+        path: '/',
+        component: () => import('../views/Home.vue'),
+      },
+    ]
+  }
+]
 
 const router = createRouter({
   // history: createWebHashHistory(),
   history: createWebHistory(),
   routes
-});
+})
 
-export default router;
+export default router
